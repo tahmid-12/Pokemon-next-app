998079f8745df443d021897d6a628759795b0809
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2025-01-26 23:59:31 +0600

    page created

--- context/globalContext.js
+++ context/globalContext.js
@@ -5,9 +5,9 @@ const GlobalContext = createContext();
 
 export const GlobalContextProvider = ({ children }) => {
 
-    const { loading, fetchPokemon, pokemonData, pokemonListDetails } = usePokemonData();
+    const { loading, fetchPokemon, pokemonData, pokemonListDetails,fetchPokemonByName, activePokemon } = usePokemonData();
 
-    return <GlobalContext.Provider value={{loading, fetchPokemon, pokemonData, pokemonListDetails}}>{children}</GlobalContext.Provider>
+    return <GlobalContext.Provider value={{loading, fetchPokemon, pokemonData, pokemonListDetails,fetchPokemonByName, activePokemon }}>{children}</GlobalContext.Provider>
 }
 
 export const useGlobalContext = () => {
